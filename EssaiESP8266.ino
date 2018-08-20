3c6299de292d0f478c721d272e7d8e3de5a175ee
pwi <paul.wysocki@ixblue.com>
2018-08-20 18:17:10 +0200

    Nettoyage du code pour préparer version autre que pour poules et création nouvelle branche

--- EssaiESP8266.ino
+++ EssaiESP8266.ino
@@ -1,5 +1,4 @@
 #include <ESP8266WiFi.h>
-#include "StepperPWI.h"		//La librairie utilisé pour piloter le moteur steps
 #include <ID-PSW.h>	/* Inclus les #define de:
 		NOMWIFI (SSID de mon réseau Wifi)
 		MDPWIFI (Mot de passe de mon réseau WiFi)
@@ -17,14 +16,6 @@
 //DEMMARRAGE
 void setup(){//_________________________SETUP__________________________________
 int Etat;
-// Paramétrage des 4 IO dédié au stepper en mode output, dédié au stepper
-pinMode(IN1,OUTPUT);
-pinMode(IN2,OUTPUT);
-pinMode(IN3,OUTPUT);
-pinMode(IN4,OUTPUT);
-// Paramétrage de la pin pour la LED et allumage
-pinMode(IN5,OUTPUT);
-digitalWrite(IN5,true);
 
 Serial.begin(115200);
 //Serial.setDebugOutput(true);	Affiche du debug sur la connexion Wifi ...
@@ -36,62 +27,43 @@ if (Etat<0) GerErreurs(Etat);
 
 void loop(void){//__________________________LOOP________________________________
 String ReponseBrute;				// Variable String récupérant RepDomotics
-String Etat24="24", Etat25="25";	// IDX 24= RetourEtatPortePoules, IDX 25= ConsignePortePoules
+String Etat24="24", Etat25="25";	// !!!!!!!!!!!!!!!! A adapter pour le besoin !!!!!!!!!!!!!
 
 //INTERROGATION DOMOTICS
-HTTPMonEsp(&ReponseBrute,"devices&rid=24");
-Etat24 = ParseJson(&ReponseBrute,String(Etat24),"Data"); //IDX 24= EtatPortePoules
-Serial.printf("\nValeur de Etat24: ");
+HTTPMonEsp(&ReponseBrute,"devices&rid=24");		// !!!!!!!!!!!!!!!! A adapter pour le besoin !!!!!!!!!!!!!
+Etat24 = ParseJson(&ReponseBrute,String(Etat24),"Data");
+Serial.printf("\nValeur de Etat24: ");			// !!!!!!!!!!!!!!!! A adapter pour le besoin !!!!!!!!!!!!!
 Serial.println(Etat24);
 ReponseBrute="";
 
-HTTPMonEsp(&ReponseBrute,"devices&rid=25");
-Etat25 = ParseJson(&ReponseBrute,String(Etat25),"Data"); //IDX 25= ConsignePortePoules
-Serial.printf("Valeur de Etat25: ");
+HTTPMonEsp(&ReponseBrute,"devices&rid=25");		// !!!!!!!!!!!!!!!! A adapter pour le besoin !!!!!!!!!!!!!
+Etat25 = ParseJson(&ReponseBrute,String(Etat25),"Data");
+Serial.printf("Valeur de Etat25: ");			// !!!!!!!!!!!!!!!! A adapter pour le besoin !!!!!!!!!!!!!
 Serial.println(Etat25);
 ReponseBrute="";
 
-//ACTION MOTEUR CONDITIONNELLE + MàJ DOMOTICS
-if (Etat24!=Etat25){
-	if (Etat25=="On"){
-		Serial.printf("Domoticz demande \"ON\" => Tourner moteur dans le sens horaire\n");
-		TournerMoteur(false,5*4095,1400);  //(Cw, NbSteps: 4095 = 1tour, Speed)
-		Serial.printf("Fin de rotation moteur.\nMaJ Domotics...\n");
-		HTTPMonEsp(&ReponseBrute, "command&param=switchlight&idx=24&switchcmd=On");
-	}else if (Etat25=="Off"){
-		Serial.print("Domoticz demande \"OFF\" => Tourner moteur dans le sens ANTI-horaire\n");
-		TournerMoteur(true,5*4095,1400);
-		Serial.printf("Fin de rotation moteur.\nMaJ Domotics...\n");
-		HTTPMonEsp(&ReponseBrute, "command&param=switchlight&idx=24&switchcmd=Off");
-	}else{
-		HTTPMonEsp(&ReponseBrute,"command&param=addlogmessage&message=!!!'Poule': Réponse Domotics Innatendu");
+//ACTION
+HTTPMonEsp(&ReponseBrute, "command&param=switchlight&idx=24&switchcmd=On");
+
+/*Si Erreur
+HTTPMonEsp(&ReponseBrute,"command&param=addlogmessage&message=!!!'Poule': Réponse Domotics Innatendu");
 		GerErreurs(-100);
-	}
-}else{
-	Serial.printf("Domoticz ne demande pas de changement.\nIDX24= %s\tIDX25=%s\n",Etat24.c_str(), Etat25.c_str());
-}
+*/
+
 
 //VERIFICATION QUE LA COMMANDE A ETE PRISE EN COMPTE
 Serial.print("\nVerification prise en compte commande par Domotics\n");
-Etat24="24", Etat25="25";	// IDX 24= RetourEtatPortePoules, IDX 25= ConsignePortePoules
+Etat24="24", Etat25="25";						// !!!!!!!!!!!!!!!! A adapter pour le besoin !!!!!!!!!!!!!
 HTTPMonEsp(&ReponseBrute,"devices&rid=24");
-Etat24 = ParseJson(&ReponseBrute,String(Etat24),"Data"); //IDX 24= RetourEtatPortePoules
-Serial.printf("\nValeur de Etat24: ");
+Etat24 = ParseJson(&ReponseBrute,String(Etat24),"Data");
+Serial.printf("\nValeur de Etat24: ");			// !!!!!!!!!!!!!!!! A adapter pour le besoin !!!!!!!!!!!!!
 Serial.println(Etat24);
 ReponseBrute="";
 
-HTTPMonEsp(&ReponseBrute,"devices&rid=25");
-Etat25 = ParseJson(&ReponseBrute,String(Etat25),"Data"); //IDX 25= ConsignePortePoules
-Serial.printf("Valeur de Etat25: ");
-Serial.println(Etat25);
-ReponseBrute="";
-
-if (Etat24!=Etat25){
-	HTTPMonEsp(&ReponseBrute,"command&param=addlogmessage&message=PouleCommandeNonPriseEnCompte");
-	GerErreurs(-200);
-	}else{
-	Serial.print("Modification bien prise en compte par Domoticz.\n");
-}
+/*Si Erreur
+HTTPMonEsp(&ReponseBrute,"command&param=addlogmessage&message=PouleCommandeNonPriseEnCompte");
+GerErreurs(-200);
+*/
 
 //DODO
 yield();		//C'est pour donner la main a la couche TCPIP avant de s'endormir.				
