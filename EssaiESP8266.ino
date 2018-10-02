a2d6be1dcf70bd1283e8a39b7bc8d7c834786127
pwi <paul.wysocki@ixblue.com>
2018-10-02 14:26:50 +0200

    Préparation pour porte

--- EssaiESP8266.ino
+++ EssaiESP8266.ino
@@ -12,43 +12,56 @@
 #include "HTTPMonEsp.h"
 #include "InitMonESPWifi.h"
 
-  
+#define IOPORTE 13			// GPIO relié au capteur d'ouverture de porte
+#define IOVERROU 12			// GPIO relié au capteur du vérou de la porte
+#define IDX_PORTE 24		// Idem pour les IDX de Domotics
+#define IDX_VERROU 25
+boolean Porte, Verrou;
+
 //DEMMARRAGE
 void setup(){//_________________________SETUP__________________________________
-int Etat;
-
+int Etat=0;
+pinMode(IOPORTE,INPUT);
+pinMode(IOVERROU,INPUT);
+Porte = digitalRead(IOPORTE);
+Verrou = digitalRead(IOVERROU);
 Serial.begin(115200);
-//Serial.setDebugOutput(true);	Affiche du debug sur la connexion Wifi ...
-delay(2000);                // Une petite pause au démmarage pour que l'affichage se passe bien sur le terminal ...
-SerialPrintParamMonEsp();	//Print sur serial des paramètres de mon ESP (aucune config)
-Etat = ConfigWifiMonEsp();
+Serial.print("\n\n");
+
+//SerialPrintParamMonEsp();	//Print sur serial des paramètres de mon ESP (aucune config)
+Etat = ConfigWifiMonEsp();	//Paramétrage Wifi et initialisation connexion
 if (Etat<0) GerErreurs(Etat);
 }
 
 void loop(void){//__________________________LOOP________________________________
-String ReponseBrute;				// Variable String récupérant RepDomotics
-String Etat24="24";	// !!!!!!!!!!!!!!!! A adapter pour le besoin !!!!!!!!!!!!!
+String ReponseBrute;		// Variable String récupérant RepDomotics
+String EtatIDX = IDX_PORTE;
 
-//INTERROGATION DOMOTICS
-HTTPMonEsp(&ReponseBrute,"devices&rid=24");		// !!!!!!!!!!!!!!!! A adapter pour le besoin !!!!!!!!!!!!!
-Etat24 = ParseJson(&ReponseBrute,String(Etat24),"Data");
-Serial.printf("\nValeur de Etat24: ");			// !!!!!!!!!!!!!!!! A adapter pour le besoin !!!!!!!!!!!!!
-Serial.println(Etat24);
+//INTERROGATION DOMOTICS sur état IDX_PORTE
+HTTPMonEsp(&ReponseBrute,"devices&rid=" & IDX_PORTE);		// ATTENTION, c'est la première fois que je fusionne la chaine de caractères avec une contante
+EtatIDX = ParseJson(&ReponseBrute,String(IDX_PORTE),"Data");	// String ParseJson(String MessageHHTP, String IDX_Cherché, String Champ recherché)
+Serial.printf("\nValeur de " & IDX_PORTE & ": ");
+Serial.println(EtatIDX);
 ReponseBrute="";
 
-//ACTION
-HTTPMonEsp(&ReponseBrute, "command&param=switchlight&idx=24&switchcmd=On");// !!!!!!!!!!!!!!!! A adapter pour le besoin !!!!!!!!!!!!!
+// A SE POSER LA QUESTION
+// Pour réduire la conso, si le modem se met/est mis en someille. Il vaut mieux monitorer les changement d'état des IO sans interroger Domotics
+// Et sur le lien avec Domotics, enchainner les requètes sur une unique connexion ?! ... pourrait avoir un intérêt
+
+if ((true == Porte && "On" == EtatIDX) || (false == Porte && "Off" == EtatIDX)){
+	//NOTHING: Domotics is up to date
+}else{
+	// La il y a eu changement, il faut mettre Domotics à jour
 
 //Si Erreur
 //HTTPMonEsp(&ReponseBrute,"command&param=addlogmessage&message=!!!'Poule': Réponse Domotics Innatendu");
 //GerErreurs(-100);
 
-
+}	
 
 //DODO
 yield();		//C'est pour donner la main a la couche TCPIP avant de s'endormir.				
-digitalWrite(IN5,false);
-Serial.print("Dodo 30mn   ^_^\n\n\n\n");
-ESP.deepSleep(1800000000,WAKE_RF_DEFAULT);	//le fameux mode deepsleep en µsec: 300 000 000 = 5mn, 1800000000 =30mn
+Serial.print("Dodo 0.5sec   ^_^\n\n\n\n");
+ESP.deepSleep(500000,WAKE_RF_DEFAULT);	//activation du mode deepsleep en µsec: 300 000 000 = 5mn, 1800000000 =30mn, 500000 = 0.5Sec
 delay(60000);	// Pause de 60sec (60000msec), mais qui ne s'excute jamais lorsque le deepSleep est réalisé
 }
