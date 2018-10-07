d39c31db0f9b0f696e0998a3e16be872e4965b8d
pwi <paul.wysocki@ixblue.com>
2018-10-07 23:17:37 +0200

    Deplacement adresse IP static passage en variable globale de RéponseBrute et EtatIDX (afin de pouvoir utiliser dans setup() Rajout dans le setup() d'une interrogation Domotics au boot et synchro avec état capteur porte Re-ecriture test condtionnel en mode if ... else if ... Rajout d'un yield() avnat la pause pour limiter le crash (... faut voir si ça marche)

--- EssaiESP8266.ino
+++ EssaiESP8266.ino
@@ -8,16 +8,17 @@
 		IPAddress IPSTATIC
 		IPAddress PASSERELLE
 		IPAddress MaskNet	*/
+IPAddress IPSTATIC(192,168,1,107);	// Adresse IP de mon équipement !!-- A ADAPTER A CHAQUE COMPILATION, surtout EN MODE TEST --!!
 #include "GerErreurs.h"
 #include "HTTPMonEsp.h"
 #include "InitMonESPWifi.h"
 
-IPAddress IPSTATIC(192,168,1,107);	// Adresse IP de mon équipement !!-- A ADAPTER A CHAQUE COMPILATION, surtout EN MODE TEST --!!
 #define IOPORTE 13			// GPIO relié au capteur d'ouverture de porte
 #define IOVERROU 12			// GPIO relié au capteur du vérou de la porte
-#define IDX_PORTE 1		// Idem pour les IDX de Domotics
+#define IDX_PORTE 7		// Idem pour les IDX de Domotics
 #define IDX_VERROU 25
 bool OldPorte, OldVerrou;
+String ReponseBrute, EtatIDX = "";
 
 //DEMMARRAGE
 void setup(){//_________________________SETUP__________________________________
@@ -29,10 +30,24 @@ OldVerrou = digitalRead(IOVERROU);
 Serial.begin(115200);
 Etat = ConfigWifiMonEsp();	//Paramétrage Wifi et initialisation connexion
 if (Etat<0) GerErreurs(Etat);
+Serial.print("Synchronisation initiale entre état porte et état DOMOTICS\n");
+EtatIDX	=	"devices&rid=";
+EtatIDX += IDX_PORTE;
+HTTPMonEsp(&ReponseBrute,EtatIDX);
+EtatIDX = ParseJson(&ReponseBrute,String(IDX_PORTE),"Data");	// String ParseJson(String MessageHHTP, String IDX_Cherché, String Champ recherché)
+ReponseBrute = "\nValeur de IDX_";
+ReponseBrute += IDX_PORTE;
+ReponseBrute += ": ";
+ReponseBrute += EtatIDX;
+Serial.println(ReponseBrute);
+Serial.print("FC OldPorte = ");
+Serial.println(OldPorte);
+ReponseBrute="";
+if (true == OldPorte && String("On") != EtatIDX)	HTTPMonEsp(&ReponseBrute, "command&param=switchlight&idx=7&switchcmd=On");		//Faut vraiment faire une concaténation avec IDX_PORTE ...
+if (false == OldPorte && String("Off") != EtatIDX)	HTTPMonEsp(&ReponseBrute, "command&param=switchlight&idx=7&switchcmd=Off");		//Faut vraiment faire une concaténation avec IDX_PORTE ...
 }
 
 void loop(void){//__________________________LOOP________________________________
-String ReponseBrute, EtatIDX = "";
 bool Porte, Verrou;
 Porte = digitalRead(IOPORTE);
 Verrou = digitalRead(IOVERROU);
@@ -55,10 +70,14 @@ if (OldPorte !=Porte){
 	Serial.println(OldPorte);
 	Serial.print("\n==> Envoie commande à DOMOTICS\n");
 	ReponseBrute="";
-	if (true == Porte && String("On") != EtatIDX)	HTTPMonEsp(&ReponseBrute, "command&param=switchlight&idx=1&switchcmd=On");
-	if (false == Porte && String("Off") != EtatIDX)	HTTPMonEsp(&ReponseBrute, "command&param=switchlight&idx=1&switchcmd=Off");
+	if (true == Porte && String("On") != EtatIDX){
+		HTTPMonEsp(&ReponseBrute, "command&param=switchlight&idx=7&switchcmd=On");		//Faut vraiment faire une concaténation avec IDX_PORTE ...
+	}else if (false == Porte && String("Off") != EtatIDX){
+		HTTPMonEsp(&ReponseBrute, "command&param=switchlight&idx=7&switchcmd=Off");		//Faut vraiment faire une concaténation avec IDX_PORTE ...
+	}
 	OldPorte = Porte;
 }
 //Pause
+yield();
 delay(500);
 }
