5582d96bb43a1878152bb28d768545740321be8e
WysoPaul <wysopaul@gmx.fr>
2019-03-02 15:28:23 +0100

    Code completement fonctionnel (enfin)

--- EssaiESP8266.ino
+++ EssaiESP8266.ino
@@ -8,113 +8,151 @@ ADC_MODE(ADC_VCC);								//Pour activer la fonction lecture tension alimentatio
 	et la déclaration de
 		const uint16_t PORT
 		IPAddress PASSERELLE
-		IPAddress MaskNet	*/
+		IPAddress MaskNet
+		IPAddress IPSTATIC				!!-- à vérifier lors de la compilation de     P R O D U C T I O N     --!!
+*/
 #include "GerErreurs.h"
 #include "HTTPMonEsp.h"
 #include "InitMonESPWifi.h"
 
-//IPAddress IPSTATIC(192,168,1,206);		// Adresse IP de mon équipement !!-- A ADAPTER A CHAQUE COMPILATION, surtout EN MODE TEST --!!
+
+
+//---------------------------
+//Macro de compilation
+//---------------------------
+	#define BAVARD
+	#define PWIDEBUG
+
+#ifdef PWIDEBUG
+	#ifndef BAVARD
+	#define BAVARD
+	#endif
+#endif
+
+
+//---------------------------
+//Macro du code
+//---------------------------
 #define IOCAPTEUR 13					// GPIO relié à l'ILS qui détecte l'ouverture de la fenetre (HIGH = Fenetre ouverte / Aimant absent)
 #define IOBUZZER 14						// GPIO relié au Buzzer
 #define IDX1 20							// IDX define dans Domotics (### a mettre à jour ###)
 #define DEEPSLEEP_DURATION 20000000	// En microseconds 300 000 000 = 5mn, 20 000 000 = 20Sec
 #define DUREE_OUVERT_MAX 3				// Nombre de cycles de DeepSLeep avec capteur actif qui entrainent une action
 
-void Notification(uint32_t* NbCycles);
+
+//---------------------------
+//Protoypes
+//---------------------------
+void Notification(char* NbCycles);
 void ReadBoolMemRtc(char Offset, boolean ValBinaire );
 void WriteBoolMemRtc(char Offset, boolean ValBinaire);
 
+
+
 void setup(){//_________________________SETUP__________________________________
-
-//Declaration
+//---------------------------
+//Declaration des variables locales
+//---------------------------
 String S_ReponseBrute = "", S_EtatIDX = "", S_PrkReboot = "", S_MessageIDX = "", S_MessageLog = "";
-int i_RetourEtatFcts = 0, i_Alim = 0;
-boolean b_EtatCapteur1 = false, b_OldEtatCapteur1 = false;
-uint32_t ui32_DureeOuvert = 0;			//C'est un 32bits afin de pouvoir le charger sans cast dans la mémoire RTC
+int i_RetourEtatFcts=0, i_Alim=0;
+char c_EtatCapteur1=0, c_OldEtatCapteur1=0, c_DureeOuvert=0;
 
-//Initialisation
+//---------------------------
+//Initialisations
+//---------------------------
 pinMode(IOCAPTEUR,INPUT_PULLUP);
 pinMode(IOBUZZER,OUTPUT);
 Serial.begin(115200);
 S_PrkReboot = ESP.getResetReason();
 i_Alim = ESP.getVcc();
-b_EtatCapteur1 = digitalRead(IOCAPTEUR);
+c_EtatCapteur1 = digitalRead(IOCAPTEUR);
 S_MessageIDX = "command&param=switchlight&idx=" + String(IDX1) + "&switchcmd=";
 S_MessageLog = "command&param=addlogmessage&message=";
-
-Serial.println("\n\n---- Variable avant lecture memoire -----");		//DEBUG
-Serial.printf("Old etat capteur: ");		//DEBUG
-Serial.println(b_OldEtatCapteur1);		//DEBUG
-Serial.printf("Nb Cycles: ");		//DEBUG
-Serial.println(ui32_DureeOuvert);		//DEBUG
+//&&&&&&&&& Il serait interessant de formellement/explicitement couper le modem/WIFI par défaut ... &&&&&&&&&
 
 
-Serial.printf("---- Lecture memoire -----");		//DEBUG
-ReadBoolMemRtc(1, b_OldEtatCapteur1);
-ESP.rtcUserMemoryRead((32 + 2), (uint32_t*) &ui32_DureeOuvert, sizeof(ui32_DureeOuvert));
+#ifdef PWIDEBUG 
+	Serial.println("\n\n## ---- Variable avant lecture memoire -----");
+	Serial.printf("## Old etat capteur: %d\n",c_OldEtatCapteur1);
+	Serial.printf("## Nb Cycles: %d\n",c_DureeOuvert);
+	Serial.println("## Lecture memoire ...");
+#endif
 
+yield();
+ESP.rtcUserMemoryRead(1, (uint32_t*) &c_OldEtatCapteur1, sizeof(c_OldEtatCapteur1));
+ESP.rtcUserMemoryRead(2, (uint32_t*) &c_DureeOuvert, sizeof(c_DureeOuvert));
 
-//DEBUG affiche sur le terminal
-Serial.printf("\n\n\n");
-Serial.printf("Raison du boot: ");
-Serial.println(S_PrkReboot);
-Serial.printf("Tension alim: ");
-Serial.println(i_Alim);
-Serial.printf("Etat capteur: ");
-Serial.println(b_EtatCapteur1);
-Serial.printf("Old etat capteur: ");
-Serial.println(b_OldEtatCapteur1);
-Serial.printf("Nb Cycles: ");
-Serial.println(ui32_DureeOuvert);
-
-
+#ifdef BAVARD
+Serial.printf("\nRaison du boot: %s\n",S_PrkReboot.c_str());
+//Serial.println(S_PrkReboot);
+Serial.printf("Tension alim: %d\n",i_Alim);
+Serial.printf("Etat capteur: %d,\n",c_EtatCapteur1);
+Serial.printf("Old etat capteur: %d\n",c_OldEtatCapteur1);
+Serial.printf("Nb Cycles: %d\n\n",c_DureeOuvert);
+delay(3000);
+#endif
 yield();
 
 //Choix actions
-if(String("Deep-Sleep Wake") == S_PrkReboot){				//Actions in case of Deepsleep wake-up:
-			if(true == b_EtatCapteur1){						//Fenêtre est fermé
-				if(b_EtatCapteur1 == b_OldEtatCapteur1){		//La fenetre est et était fermé
-					Serial.println("La fenetre est et etait FERMEE");		//DEBUG
-					//Dodo ^_^
+if(String("Deep-Sleep Wake") == S_PrkReboot){					//Actions in case of Deepsleep wake-up:
+			
+			if(1 == c_EtatCapteur1){							//La fenêtre est FERME
+				if(c_EtatCapteur1 == c_OldEtatCapteur1){
+					#ifdef BAVARD
+						Serial.println("La fenetre EST ET ETAIT fermee.\n Dodo ^_^");
+					#endif
 					ESP.deepSleep((uint32_t)DEEPSLEEP_DURATION,WAKE_NO_RFCAL);
-				}
-				else{										//On vient de fermer la fenêtre
-					Serial.println("On vient de FERMER la fenetre");		//DEBUG
-					ui32_DureeOuvert = 0;
-					b_OldEtatCapteur1 = b_EtatCapteur1;
+					}
+				else{
+					#ifdef BAVARD
+						Serial.println("On vient de FERMER la fenetre");
+					#endif
+					c_DureeOuvert=0;
+					c_OldEtatCapteur1 = c_EtatCapteur1;
 					S_MessageIDX +="Off";
 				}
-			}else{											//càd La fenetre est ouverte
-				ui32_DureeOuvert += 1;							// !!!!!!!!!!!!!!! risque de saturer la variable !!!!!!!!!!!!
-				if (ui32_DureeOuvert > DUREE_OUVERT_MAX) Notification(&ui32_DureeOuvert);
-				if(b_EtatCapteur1 == b_OldEtatCapteur1){		//càd La fenêtre est et était ouverte
-					Serial.println("La fenetre est et etait OUVERTE");		//DEBUG
-					//Dodo ^_^
+			
+			}else{												//La fenetre est OUVERTE
+				c_DureeOuvert += 1;								// !!! risque de saturer la variable: MAX 255 !!!
+				ESP.rtcUserMemoryWrite(2, (uint32_t*) &c_DureeOuvert, sizeof(c_DureeOuvert));
+				if (c_DureeOuvert > DUREE_OUVERT_MAX) Notification(&c_DureeOuvert);
+				
+				if(c_EtatCapteur1 == c_OldEtatCapteur1){
+					#ifdef BAVARD
+						Serial.println("La fenetre EST ET ETAIT ouverte.\n Dodo ^_^");
+					#endif
 					ESP.deepSleep((uint32_t)DEEPSLEEP_DURATION,WAKE_NO_RFCAL);
 				}
-				else{										//càd fenêtre était fermé et on vient de l'ouvrir
-					Serial.println("On vient d'OUVRIR la fenetre");		//DEBUG
-					b_OldEtatCapteur1 = b_EtatCapteur1;
+				else{
+					#ifdef BAVARD
+						Serial.println("On vient d'OUVRIR la fenetre");
+					#endif
+					c_OldEtatCapteur1 = c_EtatCapteur1;
 					S_MessageIDX +="On";
 				}
 			}
-}else if(String("External System") == S_PrkReboot){			//Actions in case of real reboot:
-			b_OldEtatCapteur1 = b_EtatCapteur1;
-			ui32_DureeOuvert = 0;
+}else{															//In all others cases (dont 'External System'')
+			c_OldEtatCapteur1 = c_EtatCapteur1;
+			c_DureeOuvert = 0;
 			S_MessageLog += IDX1;
-			S_MessageLog += "__Coupure_Alim__";
-			if(true == b_EtatCapteur1){
-				Serial.println("Reboot, fenêtre FERMEE");		//DEBUG
+			S_MessageLog += S_PrkReboot;
+			//S_MessageLog += "__Coupure_Alim__";
+			if(1 == c_EtatCapteur1){
+				#ifdef BAVARD
+					Serial.println("Reboot, fenêtre FERMEE");
+				#endif
 				S_MessageIDX +="Off";
 			}else{
-				Serial.println("Reboot, fenêtre OUVERTE");		//DEBUG
+				#ifdef BAVARD
+					Serial.println("Reboot, fenêtre OUVERTE");
+				#endif
 				S_MessageIDX +="On";
 			}
 }
 /*		ElseIFcase "Software Watchdog":
-			b_OldEtatCapteur1 = b_EtatCapteur1;
-			ui32_DureeOuvert = 0;
-			if(true == b_EtatCapteur1){
+			c_OldEtatCapteur1 = c_EtatCapteur1;
+			c_DureeOuvert=0;
+			if(1 == c_EtatCapteur1){
 				S_MessageIDX +="Off";
 			}else{
 				S_MessageIDX +="On";
@@ -122,70 +160,61 @@ if(String("Deep-Sleep Wake") == S_PrkReboot){				//Actions in case of Deepsleep 
 			S_MessageLog += IDX1;
 			S_MessageLog +="__W_A_T_C_H_D_O_G__"
 */			
+#ifdef BAVARD
+	Serial.printf("\nEnvois à Domotics\n");
+	Serial.println(S_MessageIDX);
+	Serial.println(S_MessageLog);
+	Serial.printf("\n");
+	delay(2000);
+#endif
 
 yield();
-i_RetourEtatFcts = ConfigWifiMonEsp();									//Paramétrage Wifi et initialisation connexion
+i_RetourEtatFcts = ConfigWifiMonEsp();							//Paramétrage Wifi et initialisation connexion
 if (i_RetourEtatFcts<0) GerErreurs(i_RetourEtatFcts);
-HTTPMonEsp(&S_ReponseBrute,S_MessageIDX);						//Màj etat IDX
-//HTTPMonEsp(&S_ReponseBrute,S_MessageIDX);						//Màj tension alim  ######### A Faire ############
+HTTPMonEsp(&S_ReponseBrute,S_MessageIDX);						//Màj de Domotics avec etat IDX
+//HTTPMonEsp(&S_ReponseBrute,S_MessageIDX);						//Màj de Domotics avec tension alim &&&&&&&&& A Faire &&&&&&&&&
 S_MessageLog.replace(" ", "");
-if (S_MessageLog != "command&param=addlogmessage&message=") HTTPMonEsp(&S_ReponseBrute,S_MessageLog);						//Ecriture message LOG
+if (S_MessageLog != "command&param=addlogmessage&message=") HTTPMonEsp(&S_ReponseBrute,S_MessageLog);	//Ecriture message LOG qd necessaire
 
+#ifdef BAVARD
+	Serial.println("\nEcriture memoire:");
+	Serial.printf("FYI: Etat Capteur: %d\n", c_EtatCapteur1);
+	Serial.printf("Old etat capteur: %d\n",c_OldEtatCapteur1);
+	Serial.printf("Nb Cycles: %d\n",c_DureeOuvert);
+	Serial.println("Puis Dodo ^_^");
+	delay(3000);
+#endif
 
-//Ecrire b_OldEtatCapteur1 et ui32_DureeOuvert dans la mémoire RTC
-Serial.println("Ecriture memoire");		//DEBUG
-Serial.printf("b_OldEtatCapteur1: ");		//DEBUG
-Serial.println(b_OldEtatCapteur1);		//DEBUG
-Serial.printf("ui32_DureeOuvert: ");		//DEBUG
-Serial.println(ui32_DureeOuvert);		//DEBUG
+ESP.rtcUserMemoryWrite(1, (uint32_t*) &c_OldEtatCapteur1, sizeof(c_OldEtatCapteur1));
+ESP.rtcUserMemoryWrite(2, (uint32_t*) &c_DureeOuvert, sizeof(c_DureeOuvert));
 
-WriteBoolMemRtc(1,b_OldEtatCapteur1);
-ESP.rtcUserMemoryWrite(32 + 1, (uint32_t*) &ui32_DureeOuvert, sizeof(ui32_DureeOuvert));
+#ifdef PWIDEBUG
+	Serial.println("\n## Relecture memoire");
+	ESP.rtcUserMemoryRead(1, (uint32_t*) &c_OldEtatCapteur1, sizeof(c_OldEtatCapteur1));
+	ESP.rtcUserMemoryRead(2, (uint32_t*) &c_DureeOuvert, sizeof(c_DureeOuvert));
+	Serial.printf("## FYI: Etat Capteur: %d\n", c_EtatCapteur1);
+	Serial.printf("## Old etat capteur: %d\n",c_OldEtatCapteur1);
+	Serial.printf("## Nb Cycles: %d\n",c_DureeOuvert);
+	delay(3000);
+#endif
 
-Serial.println("Relecture memoire");		//DEBUG
-ReadBoolMemRtc(1, b_OldEtatCapteur1);		//DEBUG
-ESP.rtcUserMemoryRead((32 + 2), (uint32_t*) &ui32_DureeOuvert, sizeof(ui32_DureeOuvert));		//DEBUG
-Serial.printf("b_OldEtatCapteur1: ");		//DEBUG
-Serial.println(b_OldEtatCapteur1);		//DEBUG
-Serial.printf("ui32_DureeOuvert: ");		//DEBUG
-Serial.println(ui32_DureeOuvert);		//DEBUG
-Serial.println("Dodo ^_^");		//DEBUG
 //Dodo ^_^
 ESP.deepSleep((uint32_t)DEEPSLEEP_DURATION,WAKE_NO_RFCAL);
-//### Réflechire au différent mode de Wake ... en fonction des endroits ou on s'endore
 }
 
 void loop(void){//__________________________LOOP________________________________
 }
 
-void Notification(uint32_t* NbCycles){
+void Notification(char* NbCycles){
 	tone(14,500);
 	delay(1000);
 	tone(14,1000);
 	delay(1000);
 	noTone(14);
-	//######### A Faire ############
+	//&&&&&&&&& A Faire &&&&&&&&&
 	//Il faut envoyer une notification a Domoticz
 	//Ou mieux traiter le critère dans domoticz et monitorer une variable dans Domoticz pour déclencher le buzzer (... peut consommer plus de batteries)
 }
-
-//Fonction pour lire et écrire un Booleen dans la mémoire RTC à la position offset + 32
-//Data stored in the first 32 blocks will be lost after performing an OTA update
-//Offset is measured in blocks of 4 bytes and can range from 0 to 127 blocks.
-//Data should be 4-byte aligned.
-void ReadBoolMemRtc(char Offset, boolean ValBinaire ){
-	uint32_t ValeurMemoire=0;
-	if(Offset>(127-32)) GerErreurs(-32760);
-	ESP.rtcUserMemoryRead(32 + Offset, (uint32_t*) &ValeurMemoire, sizeof(ValeurMemoire));
-	ValBinaire = (boolean) ValeurMemoire;	
-}
-void WriteBoolMemRtc(char Offset, boolean ValBinaire ){
-	uint32_t ValeurMemoire=0;
-	if(Offset>(127-32)) GerErreurs(-32760);
-	ValeurMemoire = (uint32_t) ValBinaire;
-	ESP.rtcUserMemoryWrite(32 + Offset, (uint32_t*) &ValeurMemoire, sizeof(ValeurMemoire));
-}
-
 
 /*  Rappel pour utiliser la fonction parse de JSON
 HTTPMonEsp(&S_ReponseBrute,EtatIDX);
